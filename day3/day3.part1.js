a3c29ccfdef9cb68caa15bbab7bcfa01bfb2c75a
corina <corina.schambacher@gmail.com>
2020-12-04 11:41:54 -0800

    started day 3

---
+++ day3/day3.part1.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+
+const readLines = async () => {
+    const data = await fs.readFile('./input', {encoding: 'utf-8'});
+    return data.split('\n');
+
+};
+
+const solve = async () => {
+    const lines = await readLines();
+    return true
+}
+
+solve().then(console.log)
