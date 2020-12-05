edddc78a586a986b869ed2ae352e75895ba70d90
corina <corina.schambacher@gmail.com>
2020-12-04 16:56:01 -0800

    day3part1

--- day3/day3.part1.js
+++ day3/day3.part1.js
@@ -1,5 +1,17 @@
 const fs = require('fs').promises;
 
+// , is an open space
+// # is a tree
+// input is repeated horizontally
+// goal start top left corner and hit the bottom with a rule for movement right 3 down 1
+// count nb of trees we hit
+
+// how to copy horizontally the forest
+//  how many columns do I need to traverse to reach bottom
+//  how to get an infinite version? => modulo? to stay within bounds
+// 
+
+
 const readLines = async () => {
     const data = await fs.readFile('./input', {encoding: 'utf-8'});
     return data.split('\n');
@@ -8,7 +20,17 @@ const readLines = async () => {
 
 const solve = async () => {
     const lines = await readLines();
-    return true
+    let width = lines[0].length
+    let num = 0
+    let ci = 0;
+    for (let ri = 0; ri < lines.length; ri++){
+        if (lines[ri][ci % width]==='#'){
+            num++
+        }
+        ci +=3
+    }
+    return num
 }
 
 solve().then(console.log)
+
---
+++ day3/day3.part2.js
@@ -0,0 +1,17 @@
+const fs = require('fs').promises;
+
+
+
+const readLines = async () => {
+    const data = await fs.readFile('./input', {encoding: 'utf-8'});
+    return data.split('\n');
+
+};
+
+const solve = async () => {
+    const lines = await readLines();
+    
+    return 
+}
+
+solve().then(console.log)
