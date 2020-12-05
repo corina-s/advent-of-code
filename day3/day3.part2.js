e40a0151c251af89542957c67f28783ee11c1dc6
corina <corina.schambacher@gmail.com>
2020-12-04 17:09:59 -0800

    day3part2

--- day3/day3.part2.js
+++ day3/day3.part2.js
@@ -10,8 +10,35 @@ const readLines = async () => {
 
 const solve = async () => {
     const lines = await readLines();
+    const slopes = [
+        [1,1],
+        [3,1],
+        [5,1],
+        [7,1],
+        [1,2],
+
+    ]
+    let result = 1
+    for(let slope of slopes){
+        const [right, down]= slope;
+        result *= count(lines,right, down)
+    }
+        
+    return result
+}
+
+const count = (lines, right, down)=> {
     
-    return 
+    let width = lines[0].length
+    let num = 0
+    let ci = 0;
+    for (let ri = 0; ri < lines.length; ri+=down){
+        if (lines[ri][ci % width]==='#'){
+            num++
+        }
+        ci +=right
+    }
+    return num
 }
 
 solve().then(console.log)
