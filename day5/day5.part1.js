b77f601daf73c611cb3f65e37f158b7f05f1a04d
corina <corina.schambacher@gmail.com>
2020-12-15 09:56:03 -0800

    binaryFind

--- day5/day5.part1.js
+++ day5/day5.part1.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises;
-
-
+// 128 rows: lo = 0 (inclusive) hi = 128 (exclusive) mid = 64(replaces hi as top)
+// 8 columns: 0 - 7
+// seat id row*8 + col
 const readLines = async () => {
     const data = await fs.readFile('./input', {encoding: 'utf-8'});
     return data.split('\n');
@@ -12,5 +13,23 @@ const solve = async () => {
     
 }
 
-solve().then(console.log)
+const binaryFind = (str) => {
+    let lo = 0;
+    let hi = 128;
+    for (let char of str){
+        const mid = Math.floor((lo + hi)/2);
+        if (char === 'F'){
+            // take the lower half
+            hi = mid;
+        }else if (char === 'B'){
+            // take the upper half
+            lo = mid;
+        }
+        
+    }
+    return lo;
+}
+
+console.log(binaryFind('FBFBBFF')) //44
+// solve().then(console.log)
 
